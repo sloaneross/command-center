9740d76ec9bf36537dec45d1719671ee4c3ce8b7
Sloane Ross <sloaneross113@gmail.com>
2022-11-17 00:14:09 -0500

    eugh

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <WeatherCard/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+// Components
+import WeatherCard from '../components/WeatherCard.vue';
+
+export default defineComponent({
+  name: 'AboutView',
+
+  components: {
+    WeatherCard,
+  },
+});
+</script>
